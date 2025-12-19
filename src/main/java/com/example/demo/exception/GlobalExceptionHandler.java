5017ee030bfd86873ccea11b34a7fe68c5358131
bala-amypo <bala@amypo.in>
2025-12-19 05:16:26 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -10,7 +10,8 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<?> hello(MethodArgumentNotValidException ex) {
         Map<String,String> map=new HashMap<>();
-        ex.getBindingResult().getFieldErrors().forEach(error->map.put(err))
+        ex.getBindingResult().getFieldErrors().forEach(error->map.put(error.getField(),error.getDefaultMessage()))
+        return new ResponseEntity<>(map,HttpStatus.)
     }
 
     @ExceptionHandler(PasswordNotMatchCri.class)
